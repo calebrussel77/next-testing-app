7464fad2807f02708d6e72772057baa6c43bc064
Caleb Russel <calebrussel77@gmail.com>
2022-04-16 13:52:19 +0200

    ci: add prettier and lint-staged on every pre-commit

--- src/components/Hello/Hello.spec.tsx
+++ src/components/Hello/Hello.spec.tsx
@@ -1,9 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import Hello from "./Hello";
+import { render, screen } from '@testing-library/react';
+import Hello from './Hello';
 
 it(`Should render "Hello world"`, () => {
   render(<Hello />);
   const myElement = screen.getByText(/Hello world/);
   expect(myElement).toBeInTheDocument();
-  // screen.debug();
 });
