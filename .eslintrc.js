e74ce29c5f071d7d1e30871a5afd2703398da170
Caleb Russel <calebrussel77@gmail.com>
2022-04-16 13:30:48 +0200

    ci: configure linting for tests

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "next",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
+  ignorePatterns: [".eslintrc.js", "**/*.config.js"],
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
--- src/components/Hello/Hello.spec.tsx
+++ src/components/Hello/Hello.spec.tsx
@@ -1,6 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Hello from "./Hello";
 
-it(`Should render "Hello world" `, () => {
+it(`Should render "Hello world"`, () => {
   render(<Hello />);
+  const myElement = screen.getByText(/Hello world/);
+  expect(myElement).toBeInTheDocument();
+  // screen.debug();
 });
