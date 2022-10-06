be8660c2a62f09016083f527b425141859d77378
Caleb Russel <calebrussel77@gmail.com>
2022-10-06 02:45:30 +0200

    test: update and refacctor counter test

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,62 +1,59 @@
-import {fireEvent, render, screen} from '@testing-library/react';
+/* eslint-disable testing-library/no-debugging-utils */
+import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import {ReactElement} from 'react';
 
 import {Counter} from './counter';
 
-// setup function
-function setup(jsx: ReactElement) {
-  return {
-    user: userEvent.setup(),
-    ...render(jsx),
-  };
-}
-
 describe(`Counter`, () => {
-  const incrementorBtn = screen.getByRole('spinbutton', {name: /incrementor/i});
-  const minusBtn = screen.getByRole('button', {
-    name: /substract to counter/i,
-  });
-  const plusBtn = screen.getByRole('button', {name: /add to counter/i});
-
   describe(`Should have default initialize value equal to 15`, () => {
     beforeEach(() => {
-      setup(<Counter defaultCount={15} />);
+      render(<Counter defaultCount={15} />);
     });
 
     it(`Should have current count value equal to 15`, () => {
-      expect(screen.getByText('Your counter is : 15')).toBeInTheDocument();
+      const counterTextDisplay = screen.getByText(/Your counter is : 15/);
+      expect(counterTextDisplay).toBeInTheDocument();
     });
 
     describe(`When the incrementor changes to 5`, () => {
+      it(`Should render incrementor count to 5`, async () => {
+        userEvent.setup();
+        const incrementorInput = screen.getByRole('spinbutton', {
+          name: /incrementor/i,
+        });
+        await userEvent.clear(incrementorInput);
+        await userEvent.type(incrementorInput, '5');
+        expect(incrementorInput).toHaveValue(5);
+      });
       describe(`When plus button is clicked`, () => {
-        beforeEach(async () => {
-          await userEvent.type(incrementorBtn, '5');
+        it(`Should render current count to 20`, async () => {
+          userEvent.setup();
+          const plusBtn = screen.getByRole('button', {name: /add to counter/i});
+          const incrementorInput = screen.getByRole('spinbutton', {
+            name: /incrementor/i,
+          });
+          await userEvent.clear(incrementorInput);
+          await userEvent.type(incrementorInput, '5');
           await userEvent.click(plusBtn);
-        });
-        it(`Should render incrementor count to 5`, () => {
-          expect(incrementorBtn).toHaveValue(Number(5));
-        });
-        it(`Should render current co unt to 20`, () => {
           const counterTextDisplay = screen.getByText(/Your counter is : 20/);
           expect(counterTextDisplay).toBeInTheDocument();
         });
       });
 
       describe(`When minus button is clicked`, () => {
-        beforeEach(() => {
-          fireEvent.change(screen.getByLabelText(/Incrementor/), {
-            target: {value: 5},
+        it(`Should render current count to 10`, async () => {
+          userEvent.setup();
+          const minusBtn = screen.getByRole('button', {
+            name: /substract to counter/i,
           });
-          fireEvent.click(
-            screen.getByRole('button', {name: 'substract to counter'})
-          );
-        });
-        it(`Should render incrementor count to 5`, () => {
-          expect(screen.getByLabelText(/Incrementor/)).toHaveValue(Number(5));
-        });
-        it(`Should render current count to 10`, () => {
-          expect(screen.getByText(/Your counter is : 10/)).toBeInTheDocument();
+          const incrementorInput = screen.getByRole('spinbutton', {
+            name: /incrementor/i,
+          });
+          await userEvent.clear(incrementorInput);
+          await userEvent.type(incrementorInput, '5');
+          await userEvent.click(minusBtn);
+          const counterTextDisplay = screen.getByText(/Your counter is : 10/);
+          expect(counterTextDisplay).toBeInTheDocument();
         });
       });
     });
@@ -64,32 +61,57 @@ describe(`Counter`, () => {
 
   describe(`Should have default initialize value equal to 0`, () => {
     beforeEach(() => {
-      setup(<Counter defaultCount={0} />);
+      render(<Counter defaultCount={0} />);
     });
 
     it(`Should render current count to 0`, () => {
-      expect(screen.getByText(/Your counter is : 0/)).toBeInTheDocument();
+      const counterTextDisplay = screen.getByText(/Your counter is : 0/);
+      expect(counterTextDisplay).toBeInTheDocument();
     });
 
     describe(`When plus button is clicked`, () => {
-      beforeEach(() => {
-        fireEvent.click(screen.getByRole('button', {name: 'add to counter'}));
-      });
-      it(`Should render current count to 1`, () => {
-        expect(screen.getByText(/Your counter is : 1/)).toBeInTheDocument();
+      it(`Should render current count to 1`, async () => {
+        userEvent.setup();
+        const plusBtn = screen.getByRole('button', {name: /add to counter/i});
+        await userEvent.click(plusBtn);
+        const counterTextDisplay = screen.getByText(/Your counter is : 1/);
+        expect(counterTextDisplay).toBeInTheDocument();
       });
     });
 
     describe(`When minus button is clicked`, () => {
-      beforeEach(() => {
-        fireEvent.click(
-          screen.getByRole('button', {name: 'substract to counter'})
-        );
+      it(`Should render current count to -1`, async () => {
+        userEvent.setup();
+        const minusBtn = screen.getByRole('button', {
+          name: /substract to counter/i,
+        });
+        await userEvent.click(minusBtn);
+        const counterTextDisplay = screen.getByText(/Your counter is : -1/);
+        expect(counterTextDisplay).toBeInTheDocument();
       });
+    });
+  });
 
-      it(`Should render current count to -1`, () => {
-        expect(screen.getByText(/Your counter is : -1/)).toBeInTheDocument();
+  describe(`When tab is pressed`, () => {
+    beforeEach(() => {
+      render(<Counter defaultCount={0} />);
+    });
+
+    it(`Should focus elements in order`, async () => {
+      userEvent.setup();
+      const incrementorInput = screen.getByRole('spinbutton', {
+        name: /incrementor/i,
       });
+      const plusBtn = screen.getByRole('button', {name: /add to counter/i});
+      const minusBtn = screen.getByRole('button', {
+        name: /substract to counter/i,
+      });
+      await userEvent.tab();
+      expect(incrementorInput).toHaveFocus();
+      await userEvent.tab();
+      expect(plusBtn).toHaveFocus();
+      await userEvent.tab();
+      expect(minusBtn).toHaveFocus();
     });
   });
 });
